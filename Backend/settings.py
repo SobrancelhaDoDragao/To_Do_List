ed47a5419439aa3f9265eb1dec13b26ee58433be
SobrancelhaDoDragao <eudson.duraes@gmail.com>
2022-07-25 15:57:20 -0300

    Preparando para o heroku

--- Backend/settings.py
+++ Backend/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-0)eq6e@6hayqq7biz5wzmi#@jr!1sm^kaod-43j16dw^(hv#w7
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
