3fbfc2847adc402f720a77ba47b74baa5a27ec58
SobrancelhaDoDragao <eudson.duraes@gmail.com>
2022-07-26 10:52:38 -0300

    ,

--- Backend/settings.py
+++ Backend/settings.py
@@ -95,7 +95,7 @@ DATABASES = {
 """
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'dcrp8krk4mbgva',
         'USER':'qqznrezqiphsrd',
         'PASSWORD':'fcaaeae3eeb06fb3b04b4f021e8265d95595e9eae8dfdbfec159d88dcdc0f8e1',
